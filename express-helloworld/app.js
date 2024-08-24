dd5ed31248e0e8599f7326a3e61e4e0a259d827a
Jayvee Macaraeg <jayvee.macaraeg@ibm.com.ph>
2024-08-25 00:31:01 +0800

    add new endpoint

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -5,6 +5,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!\n');
 });
 
+app.get('/jeboy', function (req, res) {
+  res.send('Hello Jeboy!\n');
+});
+
+app.get('/james', function (req, res) {
+  res.send('Hello James!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
