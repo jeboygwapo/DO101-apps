b05822408a4e64df4da194aeff49b789b93bd546
Jayvee Macaraeg <jayvee.macaraeg@ibm.com.ph>
2024-09-16 00:34:02 +0800

    edit version and removed shiena and janette endpoint, added jayvee enpoint

--- version/app.js
+++ version/app.js
@@ -5,26 +5,20 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is version 2 of the app. Hi Shiena Mae Gestoso!' + '\n';
+    response = 'This is version 3 of the app.!' + '\n';
 
     //send the response to the client
     res.send(response);
 
 });
 
-app.get('/shiena', function (req, res) {
-  response = 'I love you so much! Will you marry me? <3' + '\n';
+app.get('/jayvee', function (req, res) {
+  response = 'Ang pogi mo jeboy!' + '\n';
 
     //send the response to the client
     res.send(response);
 });
 
-app.get('/janette', function (req, res) {
-  response = 'Wag mo sasabihin kay imim, kasi surprisa ku sa kaniya' + '\n';
-
-    //send the response to the client
-    res.send(response);
-});
 
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
