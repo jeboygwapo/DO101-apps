f5bc0d19f173b15769694d08b458b34885f3207b
Jayvee Macaraeg <jayvee.macaraeg@ibm.com.ph>
2024-09-15 18:46:48 +0800

    add janette endpoint

--- version/app.js
+++ version/app.js
@@ -19,6 +19,13 @@ app.get('/shiena', function (req, res) {
     res.send(response);
 });
 
+app.get('/janette', function (req, res) {
+  response = 'Wag mo sasabihin kay imim, kasi surprisa ku sa kaniya' + '\n';
+
+    //send the response to the client
+    res.send(response);
+});
+
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
