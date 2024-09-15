3aaf149dcfa16b212cfd4120c8f8b5343f0d0717
Jayvee Macaraeg <jayvee.macaraeg@ibm.com.ph>
2024-09-15 18:43:49 +0800

    add shiena endpoint

--- version/app.js
+++ version/app.js
@@ -12,6 +12,13 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/shiena', function (req, res) {
+  response = 'I love you so much! Will you marry me? <3' + '\n';
+
+    //send the response to the client
+    res.send(response);
+});
+
 app.listen(8080, function () {
   console.log('Server listening on port 8080...');
 });
